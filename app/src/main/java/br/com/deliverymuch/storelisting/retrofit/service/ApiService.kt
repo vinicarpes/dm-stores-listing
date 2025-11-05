cc98973359c7534901c61cb94c9a1314c061a038
vinicarpes <vinicdf000@gmail.com>
2025-11-05 13:04:33 -0300

    refactor: update apiservice endpoint

--- app/src/main/java/br/com/deliverymuch/storelisting/retrofit/service/ApiService.kt
+++ app/src/main/java/br/com/deliverymuch/storelisting/retrofit/service/ApiService.kt
@@ -2,8 +2,9 @@ package br.com.deliverymuch.storelisting.retrofit.service
 
 import br.com.deliverymuch.storelisting.dto.RemoteStores
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
-    @GET("v6.1/companies?lat=-29.587122&lng=-55.48424&from=80&sort=distance&categories=Pizza&range=15&type=Todos")
-    suspend fun getLojas(): RemoteStores
+    @GET("v7.0/companies?lat={lat}&lng={lng}&delivery_method=ALL")
+    suspend fun getLojas(@Path("lat") lat: Double, @Path("lng") lng: Double): RemoteStores
 }
