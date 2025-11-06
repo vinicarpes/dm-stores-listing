53e3b3d11b52933a91e5d3b4b5df9ebcc8dec4b7
vinicarpes <vinicdf000@gmail.com>
2025-11-06 13:19:20 -0300

    refactor: rename classes and attributes

--- app/src/main/java/br/com/deliverymuch/storelisting/dto/RemoteCompanies.kt
+++ app/src/main/java/br/com/deliverymuch/storelisting/dto/RemoteCompanies.kt
@@ -1,12 +1,12 @@
 package br.com.deliverymuch.storelisting.dto
 
-import br.com.deliverymuch.storelisting.domain.models.Store
+import br.com.deliverymuch.storelisting.domain.models.RemoteCompany
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RemoteStores(
+data class RemoteCompanies(
     @SerialName("total_count")
     val totalCount: Int,
-    val data: List<Store>
+    val data: List<RemoteCompany>
 )
