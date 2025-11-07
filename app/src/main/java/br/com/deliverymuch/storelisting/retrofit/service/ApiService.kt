26d4eaf8e599b723a7ecfaa378c7199066dffc46
vinicarpes <vinicdf000@gmail.com>
2025-11-07 09:55:50 -0300

    refactor: modify project structure to match task requirement

--- app/src/main/java/br/com/deliverymuch/storelisting/retrofit/service/ApiService.kt
+++ app/src/main/java/br/com/deliverymuch/storelisting/retrofit/service/ApiService.kt
@@ -1,10 +1,9 @@
 package br.com.deliverymuch.storelisting.retrofit.service
 
-import br.com.deliverymuch.storelisting.dto.RemoteCompanies
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiService {
-    @GET("v6.1/companies?lat={lat}&lng={lng}type=Todos")
-    suspend fun fetchCompanies(@Path("lat") lat: Double, @Path("lng") lng: Double): RemoteCompanies
+    @GET("/ping")
+    suspend fun ping()
+
 }
