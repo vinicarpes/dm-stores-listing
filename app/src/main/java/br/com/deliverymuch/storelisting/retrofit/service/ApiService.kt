31ee423cb3bb034e784f7a27f31b87f31fe1a3ef
vinicarpes <vinicdf000@gmail.com>
2025-11-10 13:54:11 -0300

    feat: implement usecase for api requests - Create MainApp and MainActivity to run and test code - Inject use case with Hilt - Update manifest to enable internet usage

--- app/src/main/java/br/com/deliverymuch/storelisting/retrofit/service/ApiService.kt
+++ app/src/main/java/br/com/deliverymuch/storelisting/retrofit/service/ApiService.kt
@@ -2,9 +2,13 @@ package br.com.deliverymuch.storelisting.retrofit.service
 
 import br.com.deliverymuch.storelisting.dto.RemoteCompanies
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("v6.1/companies?lat={lat}&lng={lng}type=Todos")
-    suspend fun fetchCompanies(@Path("lat") lat: Double, @Path("lng") lng: Double): RemoteCompanies
+    @GET("v6.1/companies")
+    suspend fun fetchCompanies(
+        @Query("lat") lat: Double,
+        @Query("lng") lng: Double,
+        @Query("type") type: String = "Todos"
+    ): RemoteCompanies
 }
---
+++ app/src/main/java/br/com/deliverymuch/storelisting/ui/MainActivity.kt
@@ -0,0 +1,58 @@
+package br.com.deliverymuch.storelisting.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import br.com.deliverymuch.storelisting.ui.theme.DmstorelistingTheme
+import br.com.deliverymuch.storelisting.usecase.CompanyUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var companyUseCase: CompanyUseCase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DmstorelistingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DmstorelistingTheme {
+        Greeting("Android")
+    }
+}
